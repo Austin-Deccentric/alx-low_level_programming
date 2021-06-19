5355a6e36558832fd1a6948a766e8e31b072cedb
Austin-Deccentric <caugust19.ac@gmail.com>
2021-06-19 16:37:31 -0700

    Print to stdout using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ * Description: Prints a string to stdout
+ */
+int main(void)
+{
+	return (puts("Programming is like building a multilingual puzzle"));
+}
