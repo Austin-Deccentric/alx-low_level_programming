a01e942c60114c99de17a11785b9a9e5c5255707
Austin-Deccentric <caugust19.ac@gmail.com>
2021-06-25 01:11:19 +0200

    Print alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+* print_alphabet_x10- Print the alphabet in lowercase to stdout.
+*
+* Return: void
+* Description: Prints the lowercase alphabets 10 times on stdout.
+*/
+
+void print_alphabet_x10(void)
+{
+	char i;
+	int n = 0;
+
+	while (n < 10)
+	{
+		for (i = 'a'; i < 'z'; i++)
+			_putchar(i);
+
+		_putchar('\n');
+		n++;
+
+}
+
