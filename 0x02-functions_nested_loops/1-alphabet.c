6ee638e3d71579373d8c81ab9beeb94f7cad578e
Austin-Deccentric <caugust19.ac@gmail.com>
2021-06-25 00:51:52 +0200

    Print lower case alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* print_alphabet - Prints alphabets in lower case
+*
+* Description: Prints the alphabets in lower case to stdout.
+*/
+
+void print_alphabet(void)
+{
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+
+}
+
