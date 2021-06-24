30d1d9561ad23e8713cee24655039bbcdf02a8a3
Austin-Deccentric <caugust19.ac@gmail.com>
2021-06-25 00:28:09 +0200

    Print Holberton to stdout

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "hoberton.h"
 /**
 * main- Entry Point
 *
@@ -7,9 +7,31 @@
 
 int main(void)
 {
-	printf("Holberton\n");
+	char str[] = "Holberton";
+
+	print_string(str);
 
 	return (0);
 
 }
 
+/**
+ * print_string - Prints characters to stdout.
+ * @str: An array of chars to be printed.
+ *
+ * Description: This function accepts an array
+ * of chars as the only input an prints this to stdout
+ * Return: 0 (Success)
+ */
+int print_string(char str[])
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
