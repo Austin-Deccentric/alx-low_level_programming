6f72dc1c34a90292b190f8be2ec72c790dea7aa7
Austin-Deccentric <caugust19.ac@gmail.com>
2021-06-28 00:18:06 +0200

    function that adds two ints

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* add- Returns the result of adding to numbers.
+* @a: int; to be summed with @b
+* @b: int; second number
+* Return: int; The result of the summation
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+
+}
+
