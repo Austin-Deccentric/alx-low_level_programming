bfaefa8fe07fa2f0d8c5af61dc8123ec095fc2aa
Austin-Deccentric <caugust19.ac@gmail.com>
2021-06-29 00:16:10 +0200

    Returns the product of multiplying two numbers

---
+++ 0x04-more_funtions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* mul- Multiplies the two given numbers.
+* @a: First operand to be multiplied
+* @b: Second number to be multiplied.
+* Return: The result of the multiplication.
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+
+}
+
