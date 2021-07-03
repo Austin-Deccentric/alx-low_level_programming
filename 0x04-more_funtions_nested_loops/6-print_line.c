65089a12f100aaf13c7a828d699116d107e83171
Austin-Deccentric <caugust19.ac@gmail.com>
2021-07-03 06:25:12 +0200

    Print the required length of line.

---
+++ 0x04-more_funtions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* print_line- Draw a straight line @n times
+* @n: Length of the line.
+
+*/
+
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+
+	_putchar('\n');
+
+}
+
