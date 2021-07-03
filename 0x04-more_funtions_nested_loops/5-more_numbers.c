97c51a089f756f61f0510ce193de6cbd78107feb
Austin-Deccentric <caugust19.ac@gmail.com>
2021-07-03 06:06:26 +0200

    Print 0 to 14

---
+++ 0x04-more_funtions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+* more_numbers- Print numbers 0 to 14 10 times.
+*/
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 11; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 9)
+				_putchar('1');
+
+			_putchar('0' + (j % 10));
+		}
+
+		_putchar('\n');
+	}
+}
+
