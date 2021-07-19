ab128aaf7c5b68da64babf2e5fc698ff7a93568b
Austin-Deccentric <caugust19.ac@gmail.com>
2021-07-19 18:11:38 +0200

    Added header file

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,3 +1,11 @@
 int _putchar(char c);
+char *_strcpy(char *dest, char *src);
+void print_array(int *a,int n);
+void puts_half(char *str);
+void puts2(char *str);
+void rev_string(char *s);
+void print_rev(char *s);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
