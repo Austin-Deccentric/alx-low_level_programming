b88bed7b62216cb8e1246a9ff0f21e9f78e3b66a
Austin-Deccentric <caugust19.ac@gmail.com>
2021-07-19 18:23:07 +0200

    Fixed errors with function

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,19 +6,19 @@
 
 void puts_half(char *str)
 {
-	int size = 0, n = 0;
-	
+	int size = 0, n = 0,i;
+
 	while (*(str + size) != '\0')
 		size++;
-	
+
 	if (size % 2 == 0)
 		n = size / 2;
 	else
 		n = (size - 1) / 2;
 
-	for (; n < size; n++)
-		_putchar(*(str + n));
-	
+	for (i = n; i < size; i++)
+		_putchar(*(str + i));
+
 	_putchar('\n');
 
 }
