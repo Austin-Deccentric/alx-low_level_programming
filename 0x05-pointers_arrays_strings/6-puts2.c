f5ae606125fe85c6888aef12a8d2021c65b32400
Austin-Deccentric <caugust19.ac@gmail.com>
2021-07-19 16:50:09 +0200

    Print the first char and everyother char

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,13 +6,11 @@
 
 void puts2(char *str)
 {
-	int i = 1;
-
-	_putchar(*str);
+	int i = 0;
 
 	while (*(str + i) != '\0')
 	{
-		if (i % 2 == 0)
+		if ((i % 2 == 0) | (i == 0))
 			_putchar(*(str + i));
 		i++;
 	}
